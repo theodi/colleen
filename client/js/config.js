86e9a9a16c7a280eabb937cd9724a6784a6b2a1b
gavin <gavin@tracemedia.co.uk>
2014-05-06 18:07:49 +0100

    Canvas rendering stub

--- client/js/config.js
+++ client/js/config.js
@@ -1,7 +1,16 @@
-ZN.config = {}
+ZN.config = {
+    apiPath:"http://localhost:3000/",
+    zooniversePath:"http://live.zooniverse.org/",
 
-// "http://live.zooniverse.org/classifications/100";
-ZN.config.zooniversePath  = "http://live.zooniverse.org/";
-ZN.config.apiPath  = "http://localhost:3000/";
+    projects:["andromeda","asteroid","bat_detective","cancer_cells","cancer_gene_runner","condor",
+        "cyclone_center","galaxy_zoo","leaf",
+        "m83","milky_way","notes_from_nature","penguin",
+        "planet_four","plankton","radio",
+        "sea_floor","serengeti","spacewarp","sunspot",
+        "war_diary","wise",
+        "worms"]
+
+}
+
 
 
