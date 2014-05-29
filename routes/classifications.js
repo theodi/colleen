b5be107e3f8b3fea4df3ebe4e80c78bbeab89bbf
gavin <gavin@tracemedia.co.uk>
2014-05-29 18:21:06 +0100

    Error Handling PROTOCOL_CONNECTION_LOST

--- routes/classifications.js
+++ routes/classifications.js
@@ -14,8 +14,10 @@ var dbConfig = parseDbUrl(WNU_DB_URL);
 var WNU_DB_NAME = dbConfig['database'];
 
 //var WNU_DB_NAME = 'heroku_1b240db52f66cb2'
-
+/*
 var connection = mysql.createConnection(WNU_DB_URL);
+
+console.log('WNU_DB_NAME',WNU_DB_NAME);
 
 function handleDisconnect(connection) {
     connection.on('error', function(err) {
@@ -47,6 +49,35 @@ connection.connect(function(err) {
         console.log("Failed to Connect to DB",err.code);
     }
 });
+*/
+
+
+var connection;
+
+
+function handleDisconnect() {
+    connection = mysql.createConnection(WNU_DB_URL); // Recreate the connection, since
+    // the old one cannot be reused.
+
+    connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
 
 
 exports.getClassificationCount = function(req, res) {
