dc8b649fbcf7be5c2682cc46e644675b89648cca
gavin <gavin@tracemedia.co.uk>
2014-07-16 17:44:26 +0100

    Jasmine 2.0 Zoon Request test

--- spec/worker.spec.js
+++ spec/worker.spec.js
@@ -28,17 +28,17 @@ describe("function getProjectUpdatedTime", function() {
 
 
 describe("function fetchRequest", function() {
-    it("retrieve data from zoon and insert to classifications table", function() {
-        var fromMs = Date.UTC(2014,06,14,0,0,0);
-        var toMs = Date.UTC(2014,06,15,0,0,0);
 
-        //var fromMs = 1402743350000;
-        //var toMs = 1402829750000;
+    var fromMs = Date.UTC(2014,06,14,0,0,0);
+    var toMs = Date.UTC(2014,06,15,0,0,0);
 
-        //console.log('worker',worker.connect);
 
-        var connection = worker.connect();
-        console.log('worker.connection',worker.connection);
+    var connection = worker.connect();
+
+    var testStr = '[{"id":1359724,"created_at":"2014-07-14T23:59:55.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1357484,"created_at":"2014-07-14T23:59:28.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1357152,"created_at":"2014-07-14T23:59:21.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1356993,"created_at":"2014-07-14T23:59:18.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1355990,"created_at":"2014-07-14T23:59:10.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1354303,"created_at":"2014-07-14T23:58:46.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1353621,"created_at":"2014-07-14T23:58:38.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1352553,"created_at":"2014-07-14T23:58:28.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1352281,"created_at":"2014-07-14T23:58:20.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317},{"id":1351128,"created_at":"2014-07-14T23:58:11.000Z","user_id":155701,"project":"serengeti","country":"US","region":null,"city":"Lexington","latitude":42.4428,"longitude":-71.2317}]';
+
+    var requestStr = "";
+    beforeEach(function(done) {
         connection.connect(function(err) {
             if(err) {
                 console.log('Worker: error when connecting to db:', err);
@@ -46,27 +46,44 @@ describe("function fetchRequest", function() {
             }
 
             //worker.fetchRequest('galaxy_zoo',fromMs,toMs);
-            worker.fetchRequest('serengeti',fromMs,toMs);
+
+                worker.fetchRequest('serengeti',fromMs,toMs);
 
 
-            waitsFor(function() {
-                return false
-            }, "the data has potentially been retrieved", 10000);
+                setTimeout(function() {
+                    var nRecords = 10;
+                    var query = "SELECT * FROM classifications LIMIT "+nRecords;
+                    console.log('Classification query:',query);
 
-            runs(function () {
+                    connection.query(query, function(err2, rows, fields) {
 
-                var nRecords = 10;
-                connection.query("SELECT * FROM ?? LIMIT "+nRecords,['classifications'], function(err, rows, fields) {
+                        if(err2) {
+                            console.log('Classification query Error');
+                            throw err2;
+                        }
+                        requestStr = JSON.stringify(rows);
+                        console.log(requestStr);
 
 
-                    if(err) throw err;
-                    // rows
-                    expect(1).toEqual(1);
-                });
+                        done();
+                    });
+
+
+                }, 3000);
 
             });
-        });
 
 
     });
+
+    it("retrieves data from zoon and insert to classifications table", function(done) {
+
+        console.log('expect');
+        expect(requestStr).toEqual(testStr);
+
+        done();
+    });
+
+
+
 });
