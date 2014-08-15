3e5045048399d4b70e2e17b3ee4c53fca96987f1
Synchro <marcus@synchromedia.co.uk>
2014-08-15 02:10:25 +0200

    Updated benchmark configs Add db benchmark runner wrapper

--- benchmarks/run-bench.sh
+++ benchmarks/run-bench.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-/usr/bin/env JVM_ARGS="-Xms2g -Xmx2g" jmeter -t ./colleen_local_cli.jmx -n -l /tmp/jmeter-results.csv -JTEST_RESULTS_FILE=/tmp/jmeter-results.csv
+mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+echo "Saving temp data in $mytmpdir"
+/usr/bin/env JVM_ARGS="-Xms2g -Xmx2g" jmeter -t ./colleen_local_cli.jmx -n -l "${mytmpdir}/jmeter-results.csv" -JTEST_RESULTS_FILE="${mytmpdir}/jmeter-results.csv"
+rm -rf ${mytmpdir}
