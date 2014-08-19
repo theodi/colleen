eb51f00c3de2dfaae8a2790e8f2b3fa7e06aefad
jf301 <translatingnature@gmail.com>
2014-08-19 14:15:33 +0100

    Updated most SVGs to fix origin. added wise.json

--- client/js/config.js
+++ client/js/config.js
@@ -3,7 +3,7 @@ ZN.Config = {
     assetBB:{left:0,bottom:1080,right:1920,top:0,width:1920,height:1080},
 
     focusOpacity:1.0, // opacity of focused project
-    bgOpacity:0.05, // opacity of background projects
+    bgOpacity:0.2, // opacity of background projects
     focusDuration:1.5, // focus transition duration (seconds)
 
     bgScaleAnim:{"type":"scale","data":"day","sx":[0.02,0.25],"sy":[0.02,0.25],"tween":"linear","fn":"id"}, // background animation rule
