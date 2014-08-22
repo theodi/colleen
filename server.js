87df009a6999b810716b8e526a6f15e2b2c525d5
gavin <gavin@tracemedia.co.uk>
2014-08-22 16:31:29 +0100

    Add connection pooling to all routes.

--- server.js
+++ server.js
@@ -14,21 +14,30 @@ app.use('/', express.static(__dirname +'/client'));
 app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
 app.use(express.bodyParser());
 
+
+// timeseries
+app.get('/timeseries',classifications.getTimeSeries); // client
+app.get('/timeseries/from/:from/to/:to',classifications.getTimeSeriesBetweenDates); // client
+app.get('/timeseries/intervals/:intervals',classifications.getTimeSeriesIntervals);
+
+// classifications
+app.get('/classifications/from/:from/to/:to/interval/:interval', classifications.getClassificationInterval); // charts
 app.get('/classificationCount', classifications.getClassificationCount);
 app.get('/classifications/:count/offset/:offset', classifications.getLastClassifications);
-app.get('/classifications/from/:from/to/:to/interval/:interval', classifications.getClassificationInterval); // charts
 app.get('/classificationCount/latest/:seconds',classifications.getClassificationCountLatest);
+
+// analytics
+app.get('/analytics',classifications.getAnalytics);
+app.get('/analytics/totals',classifications.getAnalyticsAggregateCountries);
+
+// diagnostics
+app.get('/ping', classifications.ping); // monitor
+app.get('/dbstats',classifications.getDBstats);
+
+// generate timeseries and analytics
 //app.get('/updateAnalytics',classifications.updateAnalytics);
 //app.get('/updateTimeSeries',classifications.updateTimeSeries);
 //app.get('/updateTimeSeries/from/:from/to/:to/interval/:interval', classifications.updateTimeSeries);
-app.get('/analytics',classifications.getAnalytics);
-app.get('/analytics/totals',classifications.getAnalyticsAggregateCountries);
-app.get('/timeseries',classifications.getTimeSeries); // client
-app.get('/timeseries/intervals/:intervals',classifications.getTimeSeriesIntervals); // client
-app.get('/timeseries/from/:from/to/:to',classifications.getTimeSeriesBetweenDates); // client
-app.get('/ping', classifications.ping); // monitor
-
-app.get('/dbstats',classifications.getDBstats);
 
 var port = process.env.PORT || 3000;
 app.listen(port);
