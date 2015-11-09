996c12808a6cbb5ccbadecc49b1d7e08b81c82b3
jf301 <translatingnature@gmail.com>
2015-11-09 14:21:46 +0000

    amended app.js to remove error messages from exhib.html

--- client/js/app.js
+++ client/js/app.js
@@ -145,13 +145,17 @@ ZN.App.prototype = {
                         self.loadUrl(this.url, this.dataType, callback);
                     }
                     else {
-                        alert('Not Connected.');
+                        if(!isExhibtionApp){
+                            alert('Not Connected.');}
                     }
                     //alert('Not connect.\n Verify Network.');
                 } else if (jqXHR.status == 404) {
-                    alert('Requested page not found. [404]');
+                    if(!isExhibtionApp){
+                        alert('Requested page not found. [404]');
+                    }
                 } else if (jqXHR.status == 500) {
-                    alert('Internal Server Error [500].');
+                    if(!isExhibtionApp){
+                        alert('Internal Server Error [500].');}
                 } else if (exception === 'parsererror') {
                     alert('Requested JSON parse failed.');
                 } else {
