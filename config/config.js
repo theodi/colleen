4cf0365d7f96b143363d83017e6f30a06a325ae8
jf301 <translatingnature@gmail.com>
2015-11-09 15:21:43 +0000

    amended config.js to use stereo audio files on S3

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
 
 var soundOptions = {
 
-  baseUrl : 'http://s3.amazonaws.com/weneedus/audio/wnu',
+  baseUrl : 'http://s3.amazonaws.com/weneedus/audio-exhibition/wnu',
   //baseUrl : 'audio/wnu',
   pathToSoundset: 'client/audio/wnu',
 
