2370e6a61f6853e71643bdc86a68b84eeea0203e
jf301 <translatingnature@gmail.com>
2018-11-02 13:32:03 +0000

    animation updates, html updates

--- config/config.js
+++ config/config.js
@@ -3,9 +3,13 @@
 
 var soundOptions = {
 
-  //baseUrl : 'https://s3.amazonaws.com/weneedus/audio/wnu',
+  //baseUrl : 'https://s3.amazonaws.com/weneedus/audio/wnu', [old]
+  //baseURL : 'https://s3.amazonaws.com/wnu/audio/wnu/',
   //baseUrl : 'https://s3.amazonaws.com/weneedus/audio/wnu2',
+  //baseURL : 'https://s3.eu-west-2.amazonaws.com/wnu2/audio/wnu' [sw says works]
+  
   baseUrl : 'audio/wnu',
+  
   pathToSoundset: 'client/audio/wnu',
 
   extension : 'mp3',
