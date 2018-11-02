93e4ef34b0d09633341b404e94287a790c667b68
Stephen Wolff <stephen@maxgatedigital.com>
2018-11-02 22:25:37 +0000

    Use working S3 baseUrl, not broken one

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
 
 var soundOptions = {
 
-  baseUrl : 'https://s3.amazonaws.com/weneedus/audio/wnu',
+  baseUrl : 'https://s3.eu-west-2.amazonaws.com/wnu2/audio/wnu',
   //baseUrl : 'audio/wnu',
   pathToSoundset: 'client/audio/wnu',
 
